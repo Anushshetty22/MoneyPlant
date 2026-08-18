76e94994ebe4d8c1b5b26f6519f4a30810690239
Anush Shetty <anushshetty135@gmail.com>
2026-08-18 18:24:18 +0530

    go: add API application entry point

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,10 @@
+// Package main contains the executable entry point for the MoneyPlant API.
+package main
+
+import "log"
+
+// main is the first function executed when the API program starts.
+func main() {
+	// Use Go's standard logger so startup messages include a timestamp.
+	log.Println("MoneyPlant API starting")
+}
